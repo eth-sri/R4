b55e210b100b6332e5f97276aadf270cefb1191b
Casper Svenning Jensen <casper@svenningjensen.dk>
2013-10-26 14:48:05 +0200

    Change test script to use Python3

--- R5/test.py
+++ R5/test.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import subprocess
 import os
 import sys
+
 
 def abs_path(rel_path):
     return os.path.join(
@@ -13,26 +14,26 @@ def abs_path(rel_path):
 if __name__ == '__main__':
 
     sites = (
-	'http://cs.au.dk/~semadk/',
-	'http://google.com',
-	'http://eth.ch',
+        'http://cs.au.dk/~semadk/',
+        'http://google.com',
+        'http://eth.ch',
         'http://maps.google.com',
         'http://dir.bg',
-	'http://ford.com',
-	'http://verizon.com',
-	'http://adm.com',
-	'http://unitedhealthgroup.com',
-	'http://fedex.com'
+        'http://ford.com',
+        'http://verizon.com',
+        'http://adm.com',
+        'http://unitedhealthgroup.com',
+        'http://fedex.com'
     )
 
     if len(sys.argv) > 1:
-	sites = sys.argv[1:]
+        sites = sys.argv[1:]
 
     for site in sites:
-        print 'Testing %s' % site
+        print('Testing %s' % site)
         
-        print ' recording...'
+        print(' recording...')
         subprocess.check_call([abs_path('clients/Record/bin/record'), "-timeout", "6000", site])
         
-        print ' replaying...'
+        print(' replaying...')
         subprocess.check_call([abs_path('clients/Replay/bin/replay'), site, "/tmp/schedule.data"])
