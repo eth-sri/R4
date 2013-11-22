0527ba3e3639e7ac5ff8ac813213f748c199fa96
vraychev <veselin.raychev@gmail.com>
2013-11-22 16:58:26 +0100

    Add R5 to the build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 Tools/Scripts/build-webkit --no-netscape-plugin --qt --makeargs="-j8" --minimal
+cd R5/clients
+cd Record
+echo "Compiling R5/clients/Record..."
+qmake
+make
+cd ..
+cd Replay
+echo "Compiling R5/clients/Replay..."
+qmake
+make
