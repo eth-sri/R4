0ae4069d6f89ecefebe987336ab094cdeccb11d2
Casper Svenning Jensen <casper@svenningjensen.dk>
2014-02-13 18:37:44 +0100

    Improve reporting tool

--- R5/r5comparison/report.py
+++ R5/r5comparison/report.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
@@ -6,11 +6,38 @@ import difflib
 import re
 import shutil
 from jinja2 import Environment, PackageLoader
+import subprocess
+import concurrent.futures
+
+NUM_PROC = 7
 
 
 class HashableDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        self.ignore_properties = kwargs.pop('_ignore_properties', None)
+
+        if self.ignore_properties is None:
+            self.ignore_properties = []
+
+        super(HashableDict, self).__init__(*args, **kwargs)
+
+    def __cmp__(self, other):
+        t = self.__hash__()
+        o = other.__hash__()
+
+        if t == o:
+            return 0
+        elif t < o:
+            return -1
+        else:
+            return 1
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
     def __hash__(self):
-        return hash(tuple(sorted(self.items())))
+        return hash(tuple(sorted([item for item in self.items() if item[0] not in self.ignore_properties])))
 
 
 def parse_race(base_dir, handle):
@@ -30,7 +57,7 @@ def parse_race(base_dir, handle):
             if header == '':
                 break  # EOF reached
 
-            module, description, length = header.split(';')
+            event_action_id, module, description, length = header.split(';')
             length = int(length)
 
             if length == 0:
@@ -39,9 +66,11 @@ def parse_race(base_dir, handle):
                 details = fp.read(length)
 
             errors.append(HashableDict(
+                event_action_id=event_action_id,
                 module=module,
                 description=description,
-                details=details
+                details=details,
+                _ignore_properties=['event_action_id']
             ))
 
     stdout_file = os.path.join(handle_dir, 'stdout.txt')
@@ -57,14 +86,27 @@ def parse_race(base_dir, handle):
         'errors': errors,
         'stdout': stdout,
         'result': result_match.group(1) if result_match is not None else 'ERROR',
-        'html_state': state_match
+        'html_state': state_match is not None,
+        'race_dir': handle_dir
     }
 
 
-def compare_race(base_data, race_data):
+def generate_comparison_file(record_file, replay_file, comparison_file):
+    subprocess.check_call('compare %s %s %s' % (record_file, replay_file, comparison_file), shell=True)
+
+
+def compare_race(base_data, race_data, executor):
     """
     Outputs comparison
     """
+
+    # Compare image
+
+    if not os.path.isfile(os.path.join(race_data['race_dir'], 'comparison.png')):
+        executor.submit(generate_comparison_file,
+                        os.path.join(base_data['race_dir'], 'replay.png'),
+                        os.path.join(race_data['race_dir'], 'replay.png'),
+                        os.path.join(race_data['race_dir'], 'comparison.png'))
 
     # Errors diff
 
@@ -95,10 +137,18 @@ def compare_race(base_data, race_data):
     }
 
 
-def output_race_report(website, race, base_data, race_data, comparison, jinja, output_dir):
+def output_race_report(website, race, base_data, race_data, comparison, jinja, output_dir, input_dir):
     """
     Outputs filename of written report
     """
+
+    record_file = os.path.join(output_dir, 'record.png')
+    replay_file = os.path.join(output_dir, 'replay.png')
+    comparison_file = os.path.join(output_dir, 'comparison.png')
+
+    shutil.copy(os.path.join(input_dir, website, 'base', 'replay.png'), record_file)
+    shutil.copy(os.path.join(input_dir, website, race, 'replay.png'), replay_file)
+    shutil.copy(os.path.join(input_dir, website, race, 'comparison.png'), comparison_file)
 
     with open(os.path.join(output_dir, '%s.html' % race), 'w') as fp:
 
@@ -119,7 +169,7 @@ def append_race_index(race_index, race, base_data, race_data, comparison):
     race_index.append((race, base_data, race_data, comparison))
 
 
-def output_race_index(website, race_index, jinja, output_dir):
+def output_race_index(website, race_index, jinja, output_dir, input_dir):
 
     try:
         os.mkdir(output_dir)
@@ -127,7 +177,7 @@ def output_race_index(website, race_index, jinja, output_dir):
         pass  # folder exists
 
     for race, base_data, race_data, comparison in race_index:
-        output_race_report(website, race, base_data, race_data, comparison, jinja, output_dir)
+        output_race_report(website, race, base_data, race_data, comparison, jinja, output_dir, input_dir)
 
     with open(os.path.join(output_dir, 'index.html'), 'w') as fp:
 
@@ -145,7 +195,7 @@ def append_website_index(website_index, website, race_index):
     website_index.append((website, race_index))
 
 
-def output_website_index(website_index, jinja, output_dir):
+def output_website_index(website_index, jinja, output_dir, input_dir):
 
     try:
         os.mkdir(output_dir)
@@ -157,7 +207,7 @@ def output_website_index(website_index, jinja, output_dir):
     for website, race_index in website_index:
 
         website_dir = os.path.join(output_dir, website)
-        output_race_index(website, race_index, jinja, website_dir)
+        output_race_index(website, race_index, jinja, website_dir, input_dir)
 
         website_statistics.append({
             'website': website,
@@ -182,31 +232,35 @@ def main():
     websites = os.listdir(analysis_dir)
     website_index = init_website_index()
 
-    for website in websites:
+    with concurrent.futures.ProcessPoolExecutor(NUM_PROC) as executor:
 
-        website_dir = os.path.join(analysis_dir, website)
+        for website in websites:
 
-        races = os.listdir(website_dir)
-        race_index = init_race_index()
+            website_dir = os.path.join(analysis_dir, website)
 
-        try:
-            races.remove('base')
-            races.remove('record')
-        except ValueError:
-            print 'Error, missing base directory in output dir'
-            sys.exit(1)
+            races = os.listdir(website_dir)
+            race_index = init_race_index()
 
-        base_data = parse_race(website_dir, 'base')
+            try:
+                races.remove('base')
+                races.remove('record')
+            except ValueError:
+                print('Error, missing base directory in output dir')
+                sys.exit(1)
 
-        for race in races:
-            race_data = parse_race(website_dir, race)
-            comparison = compare_race(base_data, race_data)
-            append_race_index(race_index, race, base_data, race_data, comparison)
+            base_data = parse_race(website_dir, 'base')
 
-        append_website_index(website_index, website, race_index)
+            for race in races:
+                race_data = parse_race(website_dir, race)
+                comparison = compare_race(base_data, race_data, executor)
+                append_race_index(race_index, race, base_data, race_data, comparison)
+
+            append_website_index(website_index, website, race_index)
+
+        executor.shutdown()
 
     jinja = Environment(loader=PackageLoader('r5comparison', 'templates'))
-    print output_website_index(website_index, jinja, output_dir)
+    print(output_website_index(website_index, jinja, output_dir, analysis_dir))
 
 if __name__ == '__main__':
     main()
