79159f76a9bd94098d2f1df62a6374dbac5933a1
Casper Svenning Jensen <casper@svenningjensen.dk>
2014-06-03 18:48:22 +0200

    Improve debug build script

--- build-debug.sh
+++ build-debug.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 Tools/Scripts/build-webkit --no-netscape-plugin --qt --makeargs="-j8" --debug --minimal
+cd R4/clients
+cd Record
+echo "Compiling R4/clients/Record..."
+qmake CONFIG+=debug
+make
+cd ..
+cd Replay
+echo "Compiling R4/clients/Replay..."
+qmake CONFIG+=debug
+make
