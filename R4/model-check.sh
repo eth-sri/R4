80754b313c662a39627963e9c987ca1bece0a8ea
Casper Svenning Jensen <casper@svenningjensen.dk>
2014-09-06 19:15:16 +0200

    Add experimental param to model-check

--- R4/model-check.sh
+++ R4/model-check.sh
@@ -5,7 +5,7 @@ set -ue -o pipefail
 # INPUT HANDLING
 
 if (( ! $# > 0 )); then
-    echo "Usage: <website URL> <base dir> [--verbose] [--auto] [--depth x] [--high-time-limit]"  
+    echo "Usage: <website URL> <base dir> [--verbose] [--auto] [--depth x] [--high-time-limit] [--old-style-bound]"
     echo "Outputs result of model-checking the recording in <base dir>/record"
     exit 1
 fi
@@ -23,6 +23,7 @@ COOKIESCMD=""
 DEPTH=1
 TIMEOUT=60
 TIMEOUTCMD=""
+BOUND=""
 
 while [[ $# > 0 ]]
 do
@@ -31,6 +32,10 @@ case $1 in
     --high-time-limit)
         TIMEOUT="230000"
         TIMEOUTCMD="-scheduler_timeout_ms 800000"
+        shift
+    ;;
+    --old-style-bound)
+        BOUND="-conflict_reversal_bound_oldstyle true"
         shift
     ;;
     --auto)
@@ -83,7 +88,7 @@ echo "Running "  $PROTOCOL $URL " @ " $OUTDIR
 
 mkdir -p $OUTRUNNER
 
-CMD="/usr/bin/time -p $ER_BIN -conflict_reversal_bound=$DEPTH -in_dir=$OUTRECORD/ -in_schedule_file=$OUTRECORD/schedule.data -tmp_new_schedule_file=$OUTDIR/new_schedule.data -out_dir=$OUTDIR -tmp_error_log=$OUTDIR/out.errors.log -tmp_network_log=$OUTDIR/out.log.network.data -tmp_time_log=$OUTDIR/out.log.time.data -tmp_random_log=$OUTDIR/out.log.random.data -tmp_status_log=$OUTDIR/out.status.data -tmp_png_file=$OUTDIR/out.screenshot.png -tmp_schedule_file=$OUTDIR/out.schedule.data -tmp_stdout=$OUTDIR/stdout.txt -tmp_er_log_file=$OUTDIR/out.ER_actionlog --site=$PROTOCOL://$URL"
+CMD="/usr/bin/time -p $ER_BIN $BOUND -conflict_reversal_bound=$DEPTH -in_dir=$OUTRECORD/ -in_schedule_file=$OUTRECORD/schedule.data -tmp_new_schedule_file=$OUTDIR/new_schedule.data -out_dir=$OUTDIR -tmp_error_log=$OUTDIR/out.errors.log -tmp_network_log=$OUTDIR/out.log.network.data -tmp_time_log=$OUTDIR/out.log.time.data -tmp_random_log=$OUTDIR/out.log.random.data -tmp_status_log=$OUTDIR/out.status.data -tmp_png_file=$OUTDIR/out.screenshot.png -tmp_schedule_file=$OUTDIR/out.schedule.data -tmp_stdout=$OUTDIR/stdout.txt -tmp_er_log_file=$OUTDIR/out.ER_actionlog --site=$PROTOCOL://$URL"
 
 REPLAY_CMD="$REPLAY_BIN $AUTOCMD $VERBOSECMD $COOKIESCMD -out_dir $OUTDIR -timeout $TIMEOUT $TIMEOUTCMD -in_dir %s/ \"%s\" %s"
 
